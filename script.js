9c9c94d501d956f47c52ae0a71583e29e1a6fa47
jenaimarree <clemence.zheng1023@gmail.com>
2023-02-13 20:52:51 +0100

    Premier envoi

---
+++ script.js
@@ -0,0 +1,13 @@
+// Pour bouton radio
+
+function pourplusdevisibiliteversionlight(){
+	document.body.classList.remove('night');
+	document.body.classList.add('light');
+}
+function pourplusdevisibiliteversionnight(){
+	document.body.classList.remove('light');
+	document.body.classList.add('night');
+}
+function pourenleverleplusdevisibilite(){
+	document.body.classList.remove('night','light');
+}
